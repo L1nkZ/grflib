0b62d33f35c622ec8a30891aae9742ea7a4cd0a8
LinkZ <wanthost@gmail.com>
2019-12-30 16:49:06 +0100

    Add missing grftypes.h to the grf target Fix unused CMake option in root CMakeLists.txt

--- grf/grftypes.h
+++ grf/grftypes.h
@@ -32,8 +32,8 @@
  * certain Grf functions, so it uses the same structures as well.
  */
 
-#include <sys/types.h>
 #include <stdio.h>
+#include <sys/types.h>
 
 /*******************************************************
  * Portability macros
@@ -57,67 +57,65 @@
  * A null pointer.
  */
 
-
 /* C++ stuff */
 #ifdef __cplusplus
-	#ifdef __GNUC__
-		#define GRFINLINE inline
-	#else /* __GNUC__ */
-		#define GRFINLINE
-	#endif /* __GNUC__ */
+#ifdef __GNUC__
+#define GRFINLINE inline
+#else /* __GNUC__ */
+#define GRFINLINE
+#endif /* __GNUC__ */
 #else
-	#ifdef __GNUC__
-		#define GRFINLINE static inline
-	#else /* __GNUC__ */
-		#define GRFINLINE
-	#endif /* __GNUC__ */
+#ifdef __GNUC__
+#define GRFINLINE static inline
+#else /* __GNUC__ */
+#define GRFINLINE
+#endif /* __GNUC__ */
 #endif /* __cplusplus */
 
 /* Win32 DLL macros */
 #ifdef WIN32
-	/* Integer types */
-	#ifdef __MINGW32__
-		#include <stdint.h>
-		typedef unsigned int grf_uintptr_t; // !borf ?
-	#else
-		#ifndef _INC_WINDOWS
-			#include <windows.h>
-		#endif /* _INC_WINDOWS */
-		typedef unsigned int uint32_t;
-		typedef unsigned short uint16_t;
-		typedef unsigned char uint8_t;
+/* Integer types */
+#ifdef __MINGW32__
+#include <stdint.h>
+typedef unsigned int grf_uintptr_t; // !borf ?
+#else
+#ifndef _INC_WINDOWS
+#include <windows.h>
+#endif /* _INC_WINDOWS */
+typedef unsigned int uint32_t;
+typedef unsigned short uint16_t;
+typedef unsigned char uint8_t;
 #ifndef VC8
-		typedef unsigned int* grf_uintptr_t; // borf
+typedef unsigned int *grf_uintptr_t; // borf
 #endif
-	#endif /* __MINGW32__ */
+#endif /* __MINGW32__ */
 
-	/* Pack to 1 byte boundaries */
-	#include <pshpack1.h>
+/* Pack to 1 byte boundaries */
+#include <pshpack1.h>
 
-	#ifndef GRFLIB_STATIC
-		#ifdef GRF_BUILDING
-			#define GRFEXPORT __declspec(dllexport)
-		#else /* GRF_BUILDING */
-			#define GRFEXPORT __declspec(dllimport)
-		#endif /* GRF_BUILDING */
-	#else /* GRFLIB_STATIC */
-		#define GRFEXPORT
-	#endif /* GRFLIB_STATIC */
-#else /* WIN32 */
-	/* Integer types */
-	#include <inttypes.h>
-	typedef uintptr_t grf_uintptr_t;
-	
-	/* Pack to 1 byte boundaries */
-	#pragma pack(1)
+#ifndef GRFLIB_STATIC
+#ifdef GRF_BUILDING
+#define GRFEXPORT __declspec(dllexport)
+#else /* GRF_BUILDING */
+#define GRFEXPORT __declspec(dllimport)
+#endif /* GRF_BUILDING */
+#else  /* GRFLIB_STATIC */
+#define GRFEXPORT
+#endif /* GRFLIB_STATIC */
+#else  /* WIN32 */
+/* Integer types */
+#include <inttypes.h>
+typedef uintptr_t grf_uintptr_t;
 
-	#define GRFEXPORT
-# endif /* WIN32 */
+/* Pack to 1 byte boundaries */
+#pragma pack(1)
 
+#define GRFEXPORT
+#endif /* WIN32 */
 
 /* Make sure we have NULL, because its used all the time */
 #ifndef NULL
-	#define NULL ((void *) 0)
+#define NULL ((void *)0)
 #endif
 
 /*******************************
@@ -131,196 +129,198 @@ extern "C" {
 /** Maximum length of filenames.
  * @note GRAVITY uses 0x100 as a length for filenames.
  */
-#define GRF_NAMELEN	0x100
-
+#define GRF_NAMELEN 0x100
 
 /** Error codes return by libgrf functions */
 typedef enum {
-	/** No error, everything went well */
-	GE_SUCCESS,
+    /** No error, everything went well */
+    GE_SUCCESS,
 
-	/** Bad arguments passed to function */
-	GE_BADARGS,
+    /** Bad arguments passed to function */
+    GE_BADARGS,
 
-	/** Not a GRF file */
-	GE_INVALID,
+    /** Not a GRF file */
+    GE_INVALID,
 
-	/** Corrupted, but valid GRF file */
-	GE_CORRUPTED,
+    /** Corrupted, but valid GRF file */
+    GE_CORRUPTED,
 
-	/** Unsupported GRF version */
-	GE_NSUP,
+    /** Unsupported GRF version */
+    GE_NSUP,
 
-	/** File not found within the GRF */
-	GE_NOTFOUND,
+    /** File not found within the GRF */
+    GE_NOTFOUND,
 
-	/** Invalid index */
-	GE_INDEX,
+    /** Invalid index */
+    GE_INDEX,
 
-	/** Error information held in errno (if you're on Unix) */
-	GE_ERRNO,
+    /** Error information held in errno (if you're on Unix) */
+    GE_ERRNO,
 
-	/** Error is a zlib error, stored in extra */
-	GE_ZLIB,
+    /** Error is a zlib error, stored in extra */
+    GE_ZLIB,
 
-	/** Error is a zlib error, stored in gzFile, use gzerror to get it */
-	GE_ZLIBFILE,
+    /** Error is a zlib error, stored in gzFile, use gzerror to get it */
+    GE_ZLIBFILE,
 
-	/** File has no data (not really an "error") */
-	GE_NODATA,
+    /** File has no data (not really an "error") */
+    GE_NODATA,
 
-	/** Bad mode: tried to modify in read-only mode */
-	GE_BADMODE,
+    /** Bad mode: tried to modify in read-only mode */
+    GE_BADMODE,
 
-	/** Feature not implemented */
-	GE_NOTIMPLEMENTED
+    /** Feature not implemented */
+    GE_NOTIMPLEMENTED
 } GrfErrorType;
-
 
 /** Structure which contains error information */
 typedef struct {
-	GrfErrorType	type;		/**<  Error type */
-	uint32_t	line;		/**<  Line number in the soure code where the error took place */
-	const char	*file;		/**<  Filename of source code where the error took place. */
-	const char	*func;		/**<  Function that produced the error */
-	grf_uintptr_t	*extra;		/**< Extra information.
-					 * Stored as a grf_uintptr_t pointer (type
-					 * which can hold any pointer), but
-					 * could be anything (not even a ptr).
-					 * In most (all?) programming models
-					 * sizeof(pointer) >= sizeof(size_t)
-					 */
+    GrfErrorType type; /**<  Error type */
+    uint32_t
+        line; /**<  Line number in the soure code where the error took place */
+    const char
+        *file; /**<  Filename of source code where the error took place. */
+    const char *func;     /**<  Function that produced the error */
+    grf_uintptr_t *extra; /**< Extra information.
+                           * Stored as a grf_uintptr_t pointer (type
+                           * which can hold any pointer), but
+                           * could be anything (not even a ptr).
+                           * In most (all?) programming models
+                           * sizeof(pointer) >= sizeof(size_t)
+                           */
 } GrfError;
-
 
 /** Structure of information about files within GRFs
  *
  * We try to hold the same structure as used in GRAVITY's GRF handlers.
  *
- * @note GRAVITY's GrfFile struct is 0x114 bytes, in the same order as this structure.
+ * @note GRAVITY's GrfFile struct is 0x114 bytes, in the same order as this
+ * structure.
  */
 typedef struct _GrfFile {
-	/** Size in file.
-	 * If using any form of DES encryption, this must be a multiple of 8 (which is the block size).
-	 */
-	uint32_t compressed_len_aligned;
-	uint32_t compressed_len;		/**<  Compressed size */
-	uint32_t real_len;			/**<  Original (decompressed) file size */
-	int64_t pos;				/**< location in GRF archive */
+    /** Size in file.
+     * If using any form of DES encryption, this must be a multiple of 8 (which
+     * is the block size).
+     */
+    uint32_t compressed_len_aligned;
+    uint32_t compressed_len; /**<  Compressed size */
+    uint32_t real_len;       /**<  Original (decompressed) file size */
+    int64_t pos;             /**< location in GRF archive */
 
-	/* Directories have specific sizes and offsets, even though
-	 * no data is stored inside the GRF file
-	 */
+/* Directories have specific sizes and offsets, even though
+ * no data is stored inside the GRF file
+ */
 
-	/** GrfFile::compressed_len_aligned value used for directory entries */
-	#define GRFFILE_DIR_SZFILE	0x0714
-	/** GrfFile::compressed_len value used for directory entries */
-	#define GRFFILE_DIR_SZSMALL	0x0449
-	/** GrfFile::real_len value used for directory entries */
-	#define GRFFILE_DIR_SZORIG	0x055C
-	/** GrfFile::pos value used for directory entries */
-	#define GRFFILE_DIR_OFFSET	0x058A
+/** GrfFile::compressed_len_aligned value used for directory entries */
+#define GRFFILE_DIR_SZFILE 0x0714
+/** GrfFile::compressed_len value used for directory entries */
+#define GRFFILE_DIR_SZSMALL 0x0449
+/** GrfFile::real_len value used for directory entries */
+#define GRFFILE_DIR_SZORIG 0x055C
+/** GrfFile::pos value used for directory entries */
+#define GRFFILE_DIR_OFFSET 0x058A
 
-	/** Flags of file.
-	 * Such as whether its a file or not, and what encryption methods it uses */
-	uint8_t flags;
+    /** Flags of file.
+     * Such as whether its a file or not, and what encryption methods it uses */
+    uint8_t flags;
 
-	/* Known flags for GRF/GPF files */
-	#define GRFFILE_FLAG_FILE	0x01	/**< File entry
-						 * GrfFile::type flag to specify that
-						 * entry is a file when set (and
-						 * directory when not set)
-						 */
-	#define GRFFILE_FLAG_MIXCRYPT	0x02	/**< Encrypted
-						 * GrfFile::type flag to specify that the file
-						 * uses mixed crypto, explained in grfcrypt.h.
-						 */
-	#define GRFFILE_FLAG_0x14_DES	0x04	/**< Encrypted
-						 * GrfFile::type flag to specify that only the
-						 * first 0x14 blocks are encrypted.
-						 * Explained in grfcrypt.h
-						 */
+/* Known flags for GRF/GPF files */
+#define GRFFILE_FLAG_FILE                                                      \
+    0x01 /**< File entry                                                       \
+          * GrfFile::type flag to specify that                                 \
+          * entry is a file when set (and                                      \
+          * directory when not set)                                            \
+          */
+#define GRFFILE_FLAG_MIXCRYPT                                                  \
+    0x02 /**< Encrypted                                                        \
+          * GrfFile::type flag to specify that the file                        \
+          * uses mixed crypto, explained in grfcrypt.h.                        \
+          */
+#define GRFFILE_FLAG_0x14_DES                                                  \
+    0x04 /**< Encrypted                                                        \
+          * GrfFile::type flag to specify that only the                        \
+          * first 0x14 blocks are encrypted.                                   \
+          * Explained in grfcrypt.h                                            \
+          */
 
-	uint32_t hash;			/**<  Filename hash; used internally by grf_find() */
-	char name[GRF_NAMELEN];		/**<  Filename */
+    uint32_t hash; /**<  Filename hash; used internally by grf_find() */
+    char name[GRF_NAMELEN]; /**<  Filename */
 
-	/* This is calculated when the file is crypted, and only used
-	 * for GRFFILE_FLAG_MIXCRYPT, to determine how often to use
-	 * DES encryption.
-	 * Commented out because it doesn't appear in GRAVITY's struct
-	 */
-	/* uint32_t cycle; */
-	
-	/* Extra data (which is not found in GRAVITY's struct) */
-	char *data;			/**<  Uncompressed file data */
-	struct _GrfFile *next;		/**<  Linked list */
-	struct _GrfFile *prev;		/**<  Reverse linked list */
+    /* This is calculated when the file is crypted, and only used
+     * for GRFFILE_FLAG_MIXCRYPT, to determine how often to use
+     * DES encryption.
+     * Commented out because it doesn't appear in GRAVITY's struct
+     */
+    /* uint32_t cycle; */
+
+    /* Extra data (which is not found in GRAVITY's struct) */
+    char *data;            /**<  Uncompressed file data */
+    struct _GrfFile *next; /**<  Linked list */
+    struct _GrfFile *prev; /**<  Reverse linked list */
 } GrfFile;
-
 
 /** Macro to check if a GrfFile is a directory entry.
  *
  * @param f GrfFile struct to check
  */
-#define GRFFILE_IS_DIR(f) (((f).flags & GRFFILE_FLAG_FILE)==0 || ( \
-	((f).compressed_len_aligned == GRFFILE_DIR_SZFILE) && \
-	((f).compressed_len == GRFFILE_DIR_SZSMALL) && \
-	((f).real_len == GRFFILE_DIR_SZORIG) && \
-	((f).pos == GRFFILE_DIR_OFFSET) \
-	))
-
+#define GRFFILE_IS_DIR(f)                                                      \
+    (((f).flags & GRFFILE_FLAG_FILE) == 0 ||                                   \
+     (((f).compressed_len_aligned == GRFFILE_DIR_SZFILE) &&                    \
+      ((f).compressed_len == GRFFILE_DIR_SZSMALL) &&                           \
+      ((f).real_len == GRFFILE_DIR_SZORIG) &&                                  \
+      ((f).pos == GRFFILE_DIR_OFFSET)))
 
 /** The structure which contains information about a GRF file. */
 typedef struct {
-	char *filename;		/**<  Archive filename. */
-	int64_t len;		/**<  Size of the GRF file */
+    char *filename; /**<  Archive filename. */
+    int64_t len;    /**<  Size of the GRF file */
 
-	uint32_t type;		/**<  Archive type (GRF, RGZ, etc)
-				 *
-				 * @see GRF_TYPE_GRF
-				 * @see GRF_TYPE_RGZ
-				 */
-	uint32_t version;	/**<  Archive internal version number */
+    uint32_t type;    /**<  Archive type (GRF, RGZ, etc)
+                       *
+                       * @see GRF_TYPE_GRF
+                       * @see GRF_TYPE_RGZ
+                       */
+    uint32_t version; /**<  Archive internal version number */
 
-	uint32_t nfiles;	/**<  Number of files in the archive */
+    uint32_t nfiles; /**<  Number of files in the archive */
 
-	/** File information array.
-	 * Array which contains information for items inside the GRF file.
-	 */
-	GrfFile *files;
-	GrfFile *first;		/**<  Beginning of the linked list of files */
-	GrfFile *last;		/**< Beginning of the reverse linked list */
+    /** File information array.
+     * Array which contains information for items inside the GRF file.
+     */
+    GrfFile *files;
+    GrfFile *first; /**<  Beginning of the linked list of files */
+    GrfFile *last;  /**< Beginning of the reverse linked list */
 
-	#ifndef DOXYGEN_SKIP
-	/* Reserved space for future expansion */
-	void *_reserved1;
-	void *_reserved2;
-	void *_reserved3;
-	void *_reserved4;
-	#endif /* DOXYGEN_SKIP */
+#ifndef DOXYGEN_SKIP
+    /* Reserved space for future expansion */
+    void *_reserved1;
+    void *_reserved2;
+    void *_reserved3;
+    void *_reserved4;
+#endif /* DOXYGEN_SKIP */
 
-	/* Private fields */
-	uint8_t allowCrypt;	/**<  Internal use only - Can files be encrypted or not? */
-	FILE *f;		/**<  Internal use only */
-	uint8_t allowWrite;	/**<  Internal use only - Can Grf be modified? */
-	void *zbuf;		/**<  Internal use only - temporary buffer space */
+    /* Private fields */
+    uint8_t
+        allowCrypt; /**<  Internal use only - Can files be encrypted or not? */
+    FILE *f;        /**<  Internal use only */
+    uint8_t allowWrite; /**<  Internal use only - Can Grf be modified? */
+    void *zbuf;         /**<  Internal use only - temporary buffer space */
 
-	#ifndef DOXYGEN_SKIP
-	/* Reserved space for future expansion */
-	void *_priv_reserved1;
-	void *_priv_reserved2;
-	void *_priv_reserved3;
-	void *_priv_reserved4;
-	#endif /* DOXYGEN_SKIP */
+#ifndef DOXYGEN_SKIP
+    /* Reserved space for future expansion */
+    void *_priv_reserved1;
+    void *_priv_reserved2;
+    void *_priv_reserved3;
+    void *_priv_reserved4;
+#endif /* DOXYGEN_SKIP */
 } Grf;
 
-
 #ifdef WIN32
-	/* Undo packing */
-	#include <poppack.h>
+/* Undo packing */
+#include <poppack.h>
 #else /* WIN32 */
-	/* Undo packing */
-	#pragma pack()
+/* Undo packing */
+#pragma pack()
 #endif /* WIN32 */
 
 #ifdef __cplusplus
